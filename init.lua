04617d92c2f5203c86570748d9c253fa720f3c28
Xoconoch <93692082+Xoconoch@users.noreply.github.com>
2025-03-22 00:10:25 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,95 +1,7 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
--- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ';'
-
 vim.env.JAVA_OPTS = '-Djdk.xml.totalEntitySizeLimit=1000000'
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -105,7 +17,6 @@ vim.opt.number = true
 
 -- Set clipboard option
 vim.opt.clipboard = 'unnamedplus'
-
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
@@ -899,6 +810,72 @@ require('lazy').setup({
   },
 
   {
+    'akinsho/toggleterm.nvim',
+    config = function()
+      require('toggleterm').setup {
+        open_mapping = [[<c-t>]],
+        direction = 'horizontal', -- You can change this to "vertical" or "float"
+      }
+    end,
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup {
+        view = {
+          side = 'right', -- Sidebar on the right
+          width = 30, -- Set width
+        },
+      }
+
+      -- Ensure NvimTree stays on top of toggleterm windows
+      vim.api.nvim_create_autocmd('TermOpen', {
+        pattern = 'term://*',
+        callback = function()
+          if require('nvim-tree.view').is_visible() then
+            vim.cmd 'NvimTreeClose | NvimTreeOpen'
+          end
+        end,
+      })
+
+      -- Keybinding for toggling the sidebar open/close with Ctrl+B
+      vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+    end,
+  },
+
+  {
+    'catgoose/nvim-colorizer.lua',
+    event = 'BufReadPre',
+    opts = { -- set to setup table
+    },
+  },
+  'mateuszwieloch/automkdir.nvim', -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
+  {
+    'ya2s/nvim-cursorline',
+    config = function()
+      require('nvim-cursorline').setup {
+        cursorline = {
+          enable = true,
+          timeout = 1000,
+          number = false,
+        },
+        cursorword = {
+          enable = true,
+          min_length = 3,
+          hl = { underline = true },
+        },
+      }
+    end,
+  },
+  {
+    'wurli/visimatch.nvim',
+    opts = {
+      chars_lower_limit = 4,
+      lines_upper_limit = 100,
+    },
+  },
+  {
     'windwp/nvim-autopairs',
     event = 'InsertEnter', -- Load when entering insert mode
     config = function()
@@ -912,56 +889,71 @@ require('lazy').setup({
     end,
   },
   {
-    'akinsho/toggleterm.nvim',
+    'CRAG666/code_runner.nvim',
+    dependencies = {
+      'CRAG666/betterTerm.nvim', -- Optional: For toggle runner mode
+      'preservim/vimux', -- Optional: For tmux integration
+    },
     config = function()
-      require('toggleterm').setup {
-        open_mapping = [[<c-t>]],
-        direction = 'horizontal', -- You can change this to "vertical" or "float"
+      require('code_runner').setup {
+        mode = 'float', -- Change mode as preferred: "tab", "toggleterm", etc.
+        focus = true, -- Focus runner window after launching
+        startinsert = true, -- Start runner in insert mode if supported
+        filetype = {
+          python = function()
+            local cwd = vim.fn.getcwd()
+            local venvs = { 'venv', '.venv', 'env' }
+            for _, v in ipairs(venvs) do
+              local venv_path = cwd .. '/' .. v
+              local py_path = venv_path .. '/bin/python'
+              if vim.fn.isdirectory(venv_path) == 1 and vim.fn.filereadable(py_path) == 1 then
+                return py_path .. ' -u'
+              end
+            end
+            return 'python3 -u'
+          end,
+          java = { 'cd $dir &&', 'javac $fileName &&', 'java $fileNameWithoutExt' },
+          c = { 'cd $dir &&', 'gcc $fileName -o /tmp/$fileNameWithoutExt', '&& /tmp/$fileNameWithoutExt' },
+          cpp = { 'cd $dir &&', 'g++ $fileName -o /tmp/$fileNameWithoutExt', '&& /tmp/$fileNameWithoutExt' },
+          javascript = 'node',
+          typescript = 'deno run',
+          rust = { 'cd $dir &&', 'rustc $fileName &&', '$dir/$fileNameWithoutExt' },
+          go = 'go run',
+          lua = 'lua',
+          bash = 'bash',
+          sh = 'sh',
+          php = 'php',
+          ruby = 'ruby',
+        },
+        better_term = {
+          clean = true,
+          number = nil,
+          init = 1,
+        },
+        project = {},
       }
     end,
   },
-
   {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('nvim-tree').setup {
-        view = {
-          side = 'right', -- Sidebar on the right
-          width = 30, -- Set width
-        },
-      }
-
-      -- Ensure NvimTree is always above toggleterm
-      vim.api.nvim_create_autocmd('TermOpen', {
-        pattern = 'term://*',
-        callback = function()
-          if require('nvim-tree.view').is_visible() then
-            vim.cmd 'NvimTreeClose | NvimTreeOpen' -- Restart NvimTree on top
-          end
-        end,
-      })
-
-      -- Keymap for toggling NvimTree with Ctrl+B
-      vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-    end,
+    'ryanoasis/vim-devicons',
   },
   {
     'pocco81/auto-save.nvim',
     config = function()
       require('auto-save').setup {
-        enabled = true, -- Auto-start on launch
-        execution_message = { enabled = false }, -- Hide save messages
-        events = { 'InsertLeave', 'TextChanged' },
-        conditions = {
-          exists = true,
-          modifiable = true,
+        execution_message = {
+          message = function() -- message to print on save
+            return 'Auto saved'
+          end,
+          dim = 0.18, -- dim the color of `message`
+          cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
         },
+        trigger_events = { 'InsertLeave', 'TextChanged' },
+        enabled = true, -- Start enabled
+        debounce_delay = 2000, -- 2-second delay (2000ms)
       }
     end,
   },
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
